a799931ac3d4964fa6654d674252192111f2aab5
liming <liming@localhost>
2021-04-21 11:28:33 +0800

    Revert "优化"
    This reverts commit 543959ccff8425396cfd0b2e2ae03ed49467c886.

--- base-redis/src/main/java/com/cds/base/redis/config/RedisConfig.java
+++ base-redis/src/main/java/com/cds/base/redis/config/RedisConfig.java
@@ -7,21 +7,19 @@
  */
 package com.cds.base.redis.config;
 
-import java.io.IOException;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
@@ -36,42 +34,41 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Configuration
 @EnableCaching
 public class RedisConfig {
+    private static final int EXPIRE = 60;
 
     @Bean
-    public RedissonClient redisson() throws IOException {
-        Config config = Config.fromYAML(new ClassPathResource("redisson.yml").getInputStream());
-        RedissonClient redisson = Redisson.create(config);
-        return redisson;
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisCacheManager(RedisCacheWriter.nonLockingRedisCacheWriter(redisConnectionFactory),
+            this.getRedisCacheConfigurationWithTtl(EXPIRE), // 默认策略，未配置的 key 会使用这个
+            this.getRedisCacheConfigurationMap() // 指定 key 策略
+        );
     }
 
-    @Bean
-    public RedissonConnectionFactory redissonConnectionFactory(RedissonClient redisson) {
-        return new RedissonConnectionFactory(redisson);
+    private Map<String, RedisCacheConfiguration> getRedisCacheConfigurationMap() {
+        Map<String, RedisCacheConfiguration> redisCacheConfigurationMap = new HashMap<>();
+        // SsoCache和BasicDataCache进行过期时间配置
+        redisCacheConfigurationMap.put("messagCache", this.getRedisCacheConfigurationWithTtl(30 * 60));
+
+        // 自定义设置缓存时间
+        redisCacheConfigurationMap.put("userCache", this.getRedisCacheConfigurationWithTtl(60));
+
+        return redisCacheConfigurationMap;
     }
 
-    @Bean("redisTemplate")
-    public RedisTemplate getRedisTemplate(RedisConnectionFactory redissonConnectionFactory) {
-        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate();
-        redisTemplate.setConnectionFactory(redissonConnectionFactory);
-        redisTemplate.setValueSerializer(valueSerializer());
-        redisTemplate.setKeySerializer(keySerializer());
-        redisTemplate.setHashKeySerializer(keySerializer());
-        redisTemplate.setHashValueSerializer(valueSerializer());
-        return redisTemplate;
+    private RedisCacheConfiguration getRedisCacheConfigurationWithTtl(Integer seconds) {
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer =
+            new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+        redisCacheConfiguration = redisCacheConfiguration
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
+            .entryTtl(Duration.ofSeconds(seconds));
+
+        return redisCacheConfiguration;
     }
 
-    @Bean
-    public RedisSerializer keySerializer() {
-        return new StringRedisSerializer();
-    }
-
-    @Bean
-    public RedisSerializer valueSerializer() {
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-        return jackson2JsonRedisSerializer;
-    }
 }
